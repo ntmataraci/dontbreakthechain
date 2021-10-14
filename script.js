9d7fbe063ba43c2f7cf3c04fa084b8e043c603ee
Nevzat Mataraci <91319943+ntmataraci@users.noreply.github.com>
2021-10-14 23:49:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 let main_number=document.querySelector(".main__number")
-let y=0
-let max=0
-
-/* WHEN CLICKED ADD X */
-
-main_number.addEventListener("click",(e)=>{
-    console.log(maxRow())
-    if (+e.target.closest(".circle").innerText.replace("X","")==(maxRow()+1)){
-e.target.closest(".circle").innerHTML+=`<div class="cross">X</div>`
-    }
-if(e.target.closest(".circle").innerText=="X"){
-e.target.closest(".circle").classList.remove("cross")
-e.target.closest(".circle").innerText=""
-localStorage.setItem("notBreakChain",y-1)
-}
-})
-
+let maxNumber=+localStorage.getItem("notBreakChain")
 
 /* CIRCLE MAKER LOOP */
 for (i=1;i<100;i++){
@@ -23,35 +7,57 @@ main_number.innerHTML+=`<div class="circle">${i}</div>`
 }
 main_number.innerHTML+=`<div class="circle">00</div>`
 
+
+let circleArr=document.querySelectorAll(".circle")
+let crossedArr=document.querySelectorAll(".cross")
+circleArr.forEach((x,idx)=>{
+    x.addEventListener("click",(e)=>{
+let clicked=+e.target.innerText
+let clckControl=+e.target.parentElement.innerText.replace("X","")
+/*delete */
+if (clckControl==maxNumber && e.target.classList.value=="cross"){
+    e.target.innerHTML=""
+    e.target.classList.remove("cross")
+    localStorage.setItem("notBreakChain",maxNumber-1)
+}
+/* */
+maxQuery(clicked,e,idx)
+    })
+})
+
+
+
+/*max value*/
+function maxQuery(clicked,e){
+maxNumber=+localStorage.getItem("notBreakChain")
+if (clicked==maxNumber+1){
+localStorage.setItem("notBreakChain",maxNumber+1)
+letsCrossed()
+}else{
+}
+}
+
+/*increasing */
+letsCrossed()
+function letsCrossed(){
+    maxNumber=+localStorage.getItem("notBreakChain")
+    for (i=0;i<maxNumber;i++){
+        circleArr[i].innerHTML=+(i+1)+`<div class="cross">X</div>`
+
+    }
+}
+
+
+
+
 /* GET LOCAL FOR CROSSED DAYS */
 if (localStorage.getItem("notBreak_name")!=null){
     document.getElementById("goal").value=localStorage.getItem("notBreak_name")
 }
 
-if (+localStorage.getItem("notBreakChain")>0){
-    for (i=0;i<(+localStorage.getItem("notBreakChain"));i++){
-
-        document.querySelectorAll(".circle")[i].innerHTML+=`<div class="cross">X</div>`
-    }
-}
-
-/*MAX VALUE IN CROSSED VALUES */
-function maxRow() {
-    let row = document.querySelectorAll(".cross")
-    if(row.length==0){
-                  localStorage.setItem("notBreakChain",1)
-        return 0
-
-    }else{
-    row.forEach(x => {
-        y < +x.closest(".circle").innerText.replace("X", "") ? y = +x.closest(".circle").innerText.replace("X", "") : ""
-    })
-    localStorage.setItem("notBreakChain",y+1)
-    return y
-}
-}
 
 document.getElementById("goal").addEventListener("change",()=>{
-const myGoal=document.getElementById("goal").value
-localStorage.setItem("notBreak_name",myGoal)
-})
+    const myGoal=document.getElementById("goal").value
+    localStorage.setItem("notBreak_name",myGoal)
+    })
+    
